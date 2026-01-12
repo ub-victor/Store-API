c96b0e87db9e89cf757bbb1172b80ffad842d4a3
ub-victor <victoireushindi371@gmail.com>
2026-01-12 14:07:54 +0200

    API store set up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
