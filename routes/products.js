e8d115d0ab34ae6151729f7e6a6626719911ce81
ub-victor <victoireushindi371@gmail.com>
2026-01-12 14:05:20 +0200

    API store set up

--- routes/products.js
+++ routes/products.js
@@ -5,4 +5,4 @@ const router = express.Router();
 const {getAllProducts, getAllProductsStatic} = require('../controllers/products');
 
 router.route('/').get(getAllProducts);
-router.route('/static').get(getAllProducts);
+router.route('/static').get(getAllProductsStatic);
