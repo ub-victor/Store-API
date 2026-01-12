43d5f5f092a27d2889a135bfe3dd1270d5e05010
ub-victor <victoireushindi371@gmail.com>
2026-01-12 13:57:18 +0200

    API store set up

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.get('/', (req,res)=>{
     res.send('<h1>Store API</h1><a href="/api/v1/products"> Products route</a>')
 })
 
+app.use('/api/v1/products', )
+
 // Products route
 
 app.use(notFoundMiddleware);
