bf487261d8f1246b4424634d148335afed11f2b9
ub-victor <victoireushindi371@gmail.com>
2026-01-12 14:08:36 +0200

    API store set up

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req,res)=>{
     res.send('<h1>Store API</h1><a href="/api/v1/products"> Products route</a>')
 })
 
-app.use('/api/v1/products', )
+app.use('/api/v1/products', productsRouter)
 
 // Products route
 
