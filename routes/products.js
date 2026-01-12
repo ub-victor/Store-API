852c7ab3790d53c063afd8df1f4c456f379c02c5
ub-victor <victoireushindi371@gmail.com>
2026-01-12 14:06:25 +0200

    API store set up

--- routes/products.js
+++ routes/products.js
@@ -6,3 +6,5 @@ const {getAllProducts, getAllProductsStatic} = require('../controllers/products'
 
 router.route('/').get(getAllProducts);
 router.route('/static').get(getAllProductsStatic);
+
+module.exports = router;
