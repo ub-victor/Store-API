8503604938f58112e48c48298a82ca865986148f
ub-victor <victoireushindi371@gmail.com>
2026-01-14 04:12:50 +0200

    Cleaning my Backend into 'MVC'

--- populate.js
+++ populate.js
@@ -8,7 +8,10 @@ const jsonProducts = require('./products.json');
 const start = async () => {
     try{
         await connectDB(process.env.MONGO_URI)
+        console.log('Success!!!')
     } catch(error){
         console.log(error.message)
     }
 }
+
+start()
