753351be7e52da83df5c4813c18c7c93eeb6a381
ub-victor <victoireushindi371@gmail.com>
2026-01-14 04:19:27 +0200

    Cleaning my Backend into 'MVC'

--- populate.js
+++ populate.js
@@ -9,7 +9,7 @@ const start = async () => {
     try{
         await connectDB(process.env.MONGO_URI)
         await Product.deleteMany();
-        await Product.create()
+        await Product.create(jsonProducts);
         console.log('Success!!!');
 
     } catch(error){
