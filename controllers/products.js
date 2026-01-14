ee0600c781c37254e2fb7de389227eb3090ea188
ub-victor <victoireushindi371@gmail.com>
2026-01-14 04:52:01 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res)=>{
 
 const getAllProducts = async (req, res)=>{
     const products = await Product.find(req.query);
-    res.status(200).json({msg: 'products route'});
+    res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {getAllProducts, getAllProductsStatic}
