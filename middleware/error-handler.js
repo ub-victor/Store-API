53ef30f361bc382392ef6d6ee8617d9b0c0e7e49
ub-victor <victoireushindi371@gmail.com>
2026-01-14 03:29:37 +0200

    Cleaning my Backend into 'MVC'

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,4 +3,4 @@ const errorHandlerMiddleware = async (err, req, res, next)=>{
     return res.status(500).json({msg: "Something went wrong, please try again"});
 }
 
-module.exports = errorHandlerMiddleware;
+module.exports = errorHandlerMiddleware; 
