a0a38a0de1c2c4444533458c3856777797282e66
ub-victor <victoireushindi371@gmail.com>
2026-01-14 03:52:07 +0200

    Cleaning my Backend into 'MVC'

--- models/product.js
+++ models/product.js
@@ -4,5 +4,9 @@ const productSchema = new mongoose.Schema({
     name:{
         type: String,
         required: [true, 'Product name must be provided']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product price must be provided']
     }
 }) 
