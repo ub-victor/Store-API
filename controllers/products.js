9e451ebcbfdd1d5c7865f528610ffb7e721f7176
ub-victor <victoireushindi371@gmail.com>
2026-01-14 16:30:37 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -8,6 +8,7 @@ const getAllProductsStatic = async (req, res)=>{
 }
 
 const getAllProducts = async (req, res)=>{
+    // Here we to not use the {} because  req.query is already an object 
     const products = await Product.find(req.query);
     res.status(200).json({products, nbHits: products.length});
 }
