ddc05e64750ed7d50a291568e1ecabb3719f3030
ub-victor <victoireushindi371@gmail.com>
2026-01-14 04:50:41 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const getAllProductsStatic = async (req, res)=>{
    const products = await Product.find({
     name: 'vase table'
    })
-    res.status(200).json({products});
+    res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res)=>{
