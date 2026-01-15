5e37601ec1b461cf8f7a67fc39e760ccd3571fd0
ub-victor <victoireushindi371@gmail.com>
2026-01-15 13:44:37 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res)=>{
 
 const getAllProducts = async (req, res)=>{
     // This line is equivalent to const featured = req.query.featured;
-    const {featured, company, search} = req.query;
+    const {featured, company, name} = req.query;
     const queryObject ={}
     if (featured){
         queryObject.featured = featured == 'true' ? true: false;
