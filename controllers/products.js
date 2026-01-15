b02063360748864950fa7b3e77863b52e9f0c5f0
ub-victor <victoireushindi371@gmail.com>
2026-01-15 17:25:20 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -24,7 +24,7 @@ const getAllProducts = async (req, res)=>{
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList); 
     }else{
-        result = result.sort('createAt');
+        result = result.sort('createdAt');
     }
     console.log(queryObject);
 
