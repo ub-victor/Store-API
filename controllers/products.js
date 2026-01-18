ebaf0a89c60165b7a59eb9260bcfd4fde9f5323f
ub-victor <victoireushindi371@gmail.com>
2026-01-18 15:06:14 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -41,6 +41,7 @@ const getAllProducts = async (req, res)=>{
     
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
 
     console.log(queryObject);
     console.log('sort:', sort);
