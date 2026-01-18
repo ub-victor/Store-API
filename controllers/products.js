2f87b50de9d7bafa3b9b795e62b0a09424f40f10
ub-victor <victoireushindi371@gmail.com>
2026-01-18 14:36:35 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res)=>{
-    const products = await Product.find({}).select('name price');
+    const products = await Product.find().select('name price');
     res.status(200).json({products, nbHits: products.length});
 }
 
