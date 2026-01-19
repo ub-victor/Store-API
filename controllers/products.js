8534c851bdda7b9669b88f25de0546a7e1a3de64
ub-victor <victoireushindi371@gmail.com>
2026-01-19 10:33:08 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -39,7 +39,7 @@ const getAllProducts = async (req, res)=>{
         result= result.select(fieldsList);
     }
     
-    const page = Number(req.query.page) || 1; 
+    const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page -1) * limit;
 
