d4bb74035e48d035a806e778e57448bb9c6b95c5
ub-victor <victoireushindi371@gmail.com>
2026-01-19 16:10:06 +0200

    Cleaning my Backend into 'MVC'

--- controllers/products.js
+++ controllers/products.js
@@ -1,104 +1,87 @@
 const Product = require('../models/product');
 
-const getAllProductsStatic = async (req, res)=>{
-    const products = await Product.find({price: { $gt:30}})
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ price: { $gt: 30 } })
     .sort('price')
     .select('name price')
     .limit(4)
     .skip(1);
-    res.status(200).json({products, nbHits: products.length});
-    /*
-    Common related operators:
 
-        $lt: less than
-        $lte: less than or equal to
-        $eq: equal to
-        $ne: not equal to
-        $in: in a list of values
-        $nin: not in a list of values
-    */
-}
-
-const getAllProducts = async (req, res)=>{
-    // This line is equivalent to const featured = req.query.featured;
-    const {featured, company, name, sort, fields,numericFilters } = req.query;
-    const queryObject ={}
-    if (featured){
-        queryObject.featured = featured == 'true' ? true: false;
-    }
-    if (company){
-        queryObject.company = company;
-    }
-    if (name){
-        // Search products whose name contains the given text (case-insensitive)
-        queryObject.name = {$regex: name, $options: 'i'};
-    }
-
-      // numeric filters
-    if (numericFilters) {
-        const operatorMap = {
-        '>': '$gt',
-        '>=': '$gte',
-        '=': '$eq',
-        '<': '$lt',
-        '<=': '$lte',
-        };
-
-        const regEx = /\b(<|>|<=|>=|=)\b/g;
-        let filters = numericFilters.replace(
-        regEx,
-        (match) => `-${operatorMap[match]}-`
-        );
-
-        const options = ['price', 'rating'];
-
-        filters.split(',').forEach((item) => {
-        const [field, operator, value] = item.split('-');
-        if (options.includes(field)) {
-            queryObject[field] = { [operator]: Number(value) };
-        }
-        });
-    }
-
-  let result = Product.find(queryObject);
-
-  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
-    
 
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
 
-    let result = Product.find(queryObject);
+  // featured
+  if (featured) {
+    queryObject.featured = featured === 'true';
+  }
 
-    let sortList;
+  // company
+  if (company) {
+    queryObject.company = company;
+  }
 
-    if (sort && sort.trim() !== '') {
-    sortList = sort.split(',').join(' ');
+  // name search
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ['price', 'rating'];
+
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  // build query
+  let result = Product.find(queryObject);
+
+  // sorting
+  if (sort && sort.trim() !== '') {
+    const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
-    } else {
+  } else {
     result = result.sort('createdAt');
-    }
+  }
 
-    if(fields){
-        const fieldsList = fields.split(',').join(' ');
-        result= result.select(fieldsList);
-    }
-    
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page -1) * limit;
+  // field selection
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
 
-    result = result.skip(skip).limit(limit);
-    //23 products , if we decide to limite it to 7, we will have 4 pages 
-    console.log(queryObject);
-    console.log('sort:', sort);
-    console.log('sortList:', sortList);
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-    const products = await result;
-    // Here we to not use the {} because  req.query is already an object 
-    //const products = await Product.find(queryObject);
-    // the above line is traslated by the db like this db.products.find({ featured: true })
-    res.status(200).json({products, nbHits: products.length});
-}
+  result = result.skip(skip).limit(limit);
 
-module.exports = {getAllProducts, getAllProductsStatic};
+  const products = await result;
+
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
